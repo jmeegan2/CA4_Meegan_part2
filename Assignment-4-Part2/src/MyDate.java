59637b74a5c8b59636e68b0e72b9c7d7ff0916f7
James Meegan <jmeegan2@owlmail.harford.edu>
2021-07-16 23:46:35 -0400

    Initial commit

---
+++ Assignment-4-Part2/src/MyDate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class MyDate {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    //System.in is a standard input stream
+        System.out.print("Enter year: ");
+        int year= sc.nextInt();
+        System.out.print("Enter month: ");
+        int month= sc.nextInt();
+        System.out.print("Enter day: ");
+        int day= sc.nextInt();
+        System.out.println("Date"+ year, "/");
+    }
+
+}
