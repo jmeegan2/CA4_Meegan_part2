f6e495fcef776532d79ea9727cea48c0eac7e81c
James Meegan <jmeegan2@owlmail.harford.edu>
2021-07-17 00:40:20 -0400

    done for day

--- Assignment-4-Part2/src/MyDate.java
+++ Assignment-4-Part2/src/MyDate.java
@@ -14,7 +14,7 @@ public class MyDate
 
 
     // returning date as in required format
-    public String getDate(){
+    public String Date(){
         return month+"/"+day+"/"+year;
     }
 
@@ -28,7 +28,7 @@ public class MyDate
         obj.setDate(07,17,2021);
 
         // display date
-        System.out.println(obj.getDate());
+        System.out.println(obj.Date());
 
         System.out.println("Enter date in format mm dd yyyy");
 
@@ -37,7 +37,7 @@ public class MyDate
         int year=sc.nextInt(); //input year
 
         obj.setDate(month,day,year);
-        System.out.println(obj.getDate());
+        System.out.println(obj.Date());
 
     }
 
