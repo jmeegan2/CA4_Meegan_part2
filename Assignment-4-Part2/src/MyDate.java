9214bc72267eb58ab85677e61694068d7818bfdd
James Meegan <jmeegan2@owlmail.harford.edu>
2021-07-17 00:33:59 -0400

    setdate,getdate created

--- Assignment-4-Part2/src/MyDate.java
+++ Assignment-4-Part2/src/MyDate.java
@@ -1,15 +1,45 @@
 import java.util.*;
-public class MyDate {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);    //System.in is a standard input stream
-        System.out.print("Enter year: ");
-        int year= sc.nextInt();
-        System.out.print("Enter month: ");
-        int month= sc.nextInt();
-        System.out.print("Enter day: ");
-        int day= sc.nextInt();
-        System.out.println("Date"+ year, "/");
+public class MyDate
+{
+
+    int day,month,year;
+
+    //setting date
+    public void setDate(int m,int d,int y){
+        day = d;
+        month = m;
+        year = y;
     }
 
-}
+
+    // returning date as in required format
+    public String getDate(){
+        return month+"/"+day+"/"+year;
+    }
+
+    public static void main(String [] args) {
+        Scanner sc =new Scanner(System.in);
+
+        // instantiate object MyDateClass
+        MyDate obj = new MyDate();
+
+        //set date values
+        obj.setDate(07,17,2021);
+
+        // display date
+        System.out.println(obj.getDate());
+
+        System.out.println("Enter date in format mm dd yyyy");
+
+        int month=sc.nextInt(); //input month
+        int day=sc.nextInt(); //input date
+        int year=sc.nextInt(); //input year
+
+        obj.setDate(month,day,year);
+        System.out.println(obj.getDate());
+
+    }
+
+    } // end main
+
